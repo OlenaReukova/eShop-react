366a83d19a7787d32e06fc0aaa86773b4d122acb
Olena Reukova <kronverk01@gmail.com>
2023-04-14 23:59:49 +0100

    home image

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-
-
-
-function () {
-    return (
-        <div>
-
+function Home() {
+  return (
+    <div>
+      <div className="home">
+        <div className="home_container">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/001/263/897/original/online-shopping-and-digital-marketing-concept-vector.jpg"
+            width="1022"
+            height="300"
+            alt=""
+            className="home_image"
+          />
         </div>
-      );
+      </div>
+    </div>
+  );
 }
 
-export default ;
 export default Home;
