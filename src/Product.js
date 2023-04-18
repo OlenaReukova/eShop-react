7fca5eed2fbc37284c6f9a4a521423126da02d4e
Olena Reukova <kronverk01@gmail.com>
2023-04-19 00:00:17 +0100

    content Product
    added rating, image, button "Add to busket"

--- src/Product.js
+++ src/Product.js
@@ -8,7 +8,13 @@ function Product() {
         <div className="product_info">
           <p>Title</p>
           <p className="product_price">£30</p>
+          <div className="product_rating">****</div>
         </div>
+        <img
+          src="https://www.devittinsurance.com/wp-content/uploads/2019/06/P90322808_highRes_bmw-r-1250-gs-cosmic-scaled.jpg"
+          alt="product_img"
+        ></img>
+        <button>Add to busket</button>
       </div>
     </div>
   );
