b322966020f4593b4720a99a5b21c8b0b18f4e48
Olena Reukova <kronverk01@gmail.com>
2023-04-21 23:22:06 +0100

    updated product content

--- src/Home.js
+++ src/Home.js
@@ -6,56 +6,60 @@ function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <img src="images/home_img.jpeg" alt="" className="home__image" />
+        <img
+          src="images/Bobber_Black_hero.png"
+          alt=""
+          className="home__image"
+        />
 
         <div className="home__row">
           <Product
             id="12321341"
-            title="Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)"
-            price={11.96}
+            title="TRIUMPH BONNEVILLE AMERICA LT"
+            price={12995.0}
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+            image="images/bonnevilleamerica1.png"
           />
           <Product
             id="49538094"
-            title="IFB 30 L Convection Microwave Oven (30BRC2, Black, With Starter Kit)"
-            price={239.0}
+            title="Triumph Bonneville T120 Gold"
+            price={13100.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81D8pNFmWzL._SL1500_.jpg"
+            image="images/Triumph Bonneville T120 Gold .png"
           />
         </div>
 
         <div className="home__row">
           <Product
             id="4903850"
-            title="All the Light we Cannot See: The Breathtaking World Wide Bestseller Paperback"
-            price={199.99}
+            title="Street Scrambler Triumph"
+            price={12000.0}
             rating={3}
-            image="https://images-eu.ssl-images-amazon.com/images/I/514kNkerQ0L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+            image="images/Street Scrambler Gold.png"
           />
           <Product
             id="23445930"
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
-            price={98.99}
+            title="Triumph Bobber Gold"
+            price={12000.0}
             rating={5}
-            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            image="images/Triumph Bobber Gold.jpg"
           />
           <Product
             id="3254354345"
-            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
-            price={598.99}
+            title="TTHRUXTON RS CHROME"
+            price={14500.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            image="images/thruxton-rs-chrome.png"
           />
         </div>
 
         <div className="home__row">
           <Product
             id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
+            title="Triumph Bonneville T100"
+            price={12900.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            image="images/T100-Blue-LHS-800x560.png"
           />
         </div>
       </div>
