8ec9e2f25f355fda951293f34ce9bd5fbdc35658
Olena Reukova <kronverk01@gmail.com>
2023-09-07 01:41:34 +0300

    checkoutPage

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import Subtotal from './Subtotal';
 
 function Checkout() {
-  return <div></div>;
+  return (
+    <div className='checkout'>
+      <div className='checkout__left'>
+        <img
+          src='https://imotorbike.my/news/wp-content/uploads/2021/03/Project-Triumph-TE1-design-2.jpg'
+          alt=''
+          className='checkout__ad'
+        />
+      </div>
+      <h2 className='checkout__title'>Your Shopping Basket</h2>
+      <CheckoutProduct />
+      <div className='checkout__right'>
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './CheckoutProduct.css';
+
+function CheckoutProduct() {
+  return (
+    <div className='CheckoutProduct'>
+      <img
+        src='https://imotorbike.my/news/wp-content/uploads/2021/03/Project-Triumph-TE1-design-2.jpg'
+        alt=''
+        className='checkoutProduct__image'
+      />
+      <div className='checkoutProduct__info'>
+        <p className='checkoutProduct__title'>nnnn</p>
+        <p className='checkoutProduct__price'>
+          <small>$</small>
+          <strong>20</strong>
+        </p>
+        <div className='checkoutProduct__rating'>⭐⭐</div>
+        <button>Remove from Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
