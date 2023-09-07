23795b3c65c9e9dac43fd47fb0a079a34d868725
Olena Reukova <kronverk01@gmail.com>
2023-09-07 23:22:36 +0300

    StateProvider
    created a data layer to use it for checkout (actually we can use the data from the data layer in any component)

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,31 @@
-import React from "react";
-import "./Product.css";
+import React from 'react';
+import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, price, rating, image }) {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
-    <div className="product">
-      <div className="product_info">
+    <div className='product'>
+      <div className='product_info'>
         <p>{title}</p>
-        <p className="product_price">
+        <p className='product_price'>
           <small>£</small>
           <strong>{price}</strong>
         </p>
-        <div className="product_rating">
+        <div className='product_rating'>
           {Array(rating) //rating on the webpage dinamically filled
             .fill()
             .map((_, i) => (
@@ -18,8 +33,8 @@ function Product({ id, title, price, rating, image }) {
             ))}
         </div>
       </div>
-      <img src={image} alt=""></img>
-      <button>Add to busket</button>
+      <img src={image} alt='' />
+      <button onClick={addToBasket}>Add to busket</button>
     </div>
   );
 }
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,15 +1,14 @@
-import { ChildCareRounded } from '@mui/icons-material';
 import React, { createContext, useContext, useReducer } from 'react';
 
-//prepare the data layer
+// Prepares the dataLayer
 export const StateContext = createContext();
 
-//wrap app and provide the data layer
-export const StateProvider = ({ reducer, initialState, children }) => {
+// Wrap our app and provide the Data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {ChildCareRounded}
-  </StateContext.Provider>;
-};
+    {children}
+  </StateContext.Provider>
+);
 
-//Pull info from the data layer
+// Pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
