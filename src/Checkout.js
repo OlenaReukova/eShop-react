d5817e4839c1b32343c4f5245c087c328f1bdca4
Olena Reukova <kronverk01@gmail.com>
2024-02-21 00:45:54 +0000

    responsive css for Checkout Component

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,30 +5,32 @@ import Subtotal from './Subtotal';
 import { useStateValue } from './StateProvider';
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket }] = useStateValue();
   return (
-    <div className='checkout'>
-      <div className='checkout__left'>
-        <img
-          src='images/ad_electric200px.jpg'
-          alt=''
-          className='checkout__ad'
-        />
-        <div>
-          <h2 className='checkout__title'>Your Shopping Basket</h2>
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+    <div className='wraper'>
+      <div className='checkout'>
+        <div className='checkout__left'>
+          <img
+            src='images/ad_electric200px.jpg'
+            alt=''
+            className='checkout__ad'
+          />
+          <div className='checkout__section'>
+            <h2 className='checkout__title'>Your Shopping Basket</h2>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className='checkout__right'>
-        <Subtotal />
+        <div className='checkout__right'>
+          <Subtotal />
+        </div>
       </div>
     </div>
   );
