cc23e2153e285cd596121e7c0ea8d4e43ea0461e
Olena Reukova <kronverk01@gmail.com>
2024-06-27 13:54:37 +0100

    header MUI

--- src/Header.js
+++ src/Header.js
@@ -1,35 +1,186 @@
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Avatar from '@mui/material/Avatar';
+import { Link, useNavigate } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
-function Header() {
-  const [{ basket }] = useStateValue();
+const pages = ['MOTORCYCLES', 'ACCESSORIES', 'OWNERS'];
+const settings = ['Login', 'Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [{ basket, user }] = useStateValue(); // Assuming user is available in state
+  const navigate = useNavigate();
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleMenuItemClick = (setting) => {
+    handleCloseUserMenu();
+    if (setting === 'Login') {
+      navigate('/login');
+    } else if (setting === 'Logout') {
+      auth.signOut();
+      navigate('/');
+    } else {
+      navigate(`/${setting.toLowerCase()}`);
+    }
+  };
+
   return (
-    <div className='header'>
-      <Link to='/' style={{ textDecoration: 'none' }}>
-        <div className='header_logo'>
-          <img src='images/logo.jpg' alt='' />
-          <h2 className='header_logoTitle'>TRIUMPH WORLD</h2>
-        </div>
-      </Link>
-      <div className='header_nav'>
-        <Link to='/login' style={{ textDecoration: 'none' }}>
-          <div className='nav_item'>
-            <span className='nav_itemLineTwo'>Sigh In</span>
-          </div>
-        </Link>
-        <Link to='/checkout' style={{ textDecoration: 'none' }}>
-          <div className='nav_itemBasket'>
-            <img src='images/cart.png' alt='' />
-            <span className='nav_itemLineTwo nav_basketCount'>
-              {basket.length}
-            </span>
-          </div>
-        </Link>
-      </div>
-    </div>
+    <AppBar
+      position='fixed'
+      sx={{ top: 0, backgroundColor: 'black', zIndex: 1100 }}>
+      <Container maxWidth='xl'>
+        <Toolbar disableGutters>
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
+            <img src='./images/logo.jpg' alt='Logo' style={{ height: 40 }} />
+          </Box>
+          <Typography
+            variant='h3'
+            noWrap
+            component={Link}
+            to='/'
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}>
+            TRIUMPH WORLD
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size='large'
+              aria-label='menu'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign='center'>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            variant='h5'
+            noWrap
+            component={Link}
+            to='/'
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontWeight: 700,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}>
+            TRIUMPH WORLD
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                component={Link}
+                to={`/${page.toLowerCase()}`}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+                onClick={handleCloseNavMenu}>
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
+            <Button component={Link} to='/checkout' sx={{ color: 'white' }}>
+              <img
+                src='images/cart.png'
+                alt='Cart'
+                style={{ height: 24, marginRight: 8 }}
+              />
+              <Typography variant='body2'>{basket?.length}</Typography>
+            </Button>
+            <Tooltip title='Open settings'>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt='User Avatar' src='/static/images/avatar/2.jpg' />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}>
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                  onClick={() => handleMenuItemClick(setting)}>
+                  <Typography textAlign='center'>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
-export default Header;
+export default ResponsiveAppBar;
