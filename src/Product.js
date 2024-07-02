9c75a82804e1e4c5205fafac1ab95d6bcfa9f98c
Olena Reukova <kronverk01@gmail.com>
2024-07-02 14:37:39 +0100

    Product MUI

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import './Product.css';
+import Grid from '@mui/material/Unstable_Grid2';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+  Box,
+} from '@mui/material';
 import { useStateValue } from './StateProvider';
 
 function Product({ id, title, price, rating, image }) {
@@ -19,24 +27,43 @@ function Product({ id, title, price, rating, image }) {
   };
 
   return (
-    <div className='product'>
-      <div className='product_info'>
-        <p className='title'>{title}</p>
-        <p className='product_price'>
-          <small>£</small>
-          <strong>{price}</strong>
-        </p>
-        <div className='product_rating'>
-          {Array(rating) //rating on the webpage dinamically filled
-            .fill()
-            .map((_, i) => (
-              <p>⭐</p>
-            ))}
-        </div>
-      </div>
-      <img src={image} alt='' />
-      <button onClick={addToBasket}>Add to busket</button>
-    </div>
+    <Box sx={{ flexGrow: 1 }} minHeight={600}>
+      <Grid container spacing={1} minHeight={600}>
+        <Grid
+          xs={12}
+          sm={12}
+          md={4}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}></Grid>
+        <Card sx={{ maxWidth: 345, margin: 2 }}>
+          <CardMedia component='img' height='140' image={image} alt={title} />
+          <CardContent>
+            <Typography gutterBottom variant='h6' component='div'>
+              {title}
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+              <strong>£{price}</strong>
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+              {Array(rating)
+                .fill()
+                .map((_, i) => (
+                  <Typography key={i} variant='body2' color='text.secondary'>
+                    ⭐
+                  </Typography>
+                ))}
+            </Box>
+          </CardContent>
+          <Button variant='contained' color='primary' onClick={addToBasket}>
+            Add to basket
+          </Button>
+        </Card>
+      </Grid>
+    </Box>
   );
 }
+
 export default Product;
