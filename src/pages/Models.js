44b514e7f02112736a2d5b5a1e0d2a29a7678895
Olena Reukova <kronverk01@gmail.com>
2024-07-30 17:12:51 +0100

    Models page
    added Models page

--- src/pages/Models.js
+++ src/pages/Models.js
@@ -4,55 +4,65 @@ import { Box, Typography } from '@mui/material';
 export default function Models() {
   return (
     <Box
-      className='models'
       sx={{
-        position: 'absolute',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        width: '100%',
+        padding: 0,
       }}>
       <Box
-        className='models__container'
+        className='models'
         sx={{
-          position: 'absolut',
-          backgroundColor: 'black',
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
           width: '100%',
-          maxWidth: '100%',
-          overflow: 'hidden',
+          alignItems: 'center',
+          height: '100vh',
         }}>
         <Box
-          component='img'
-          src='images/motorbike.jpg'
-          alt='Home Image'
+          className='models__container'
           sx={{
+            position: 'absolute',
+            backgroundColor: 'black',
             width: '100%',
             height: '100vh',
-            objectFit: 'cover',
-          }}
-          className='models__image'
-        />
-      </Box>
-      <Box
-        sx={{
-          position: 'absolute',
-          top: { lg: '50%', xs: '50%' },
-          left: { lg: '25%', xs: '50%' },
-          transform: 'translate(-50%, -50%)',
-          color: 'white',
-          px: 2,
-          width: { lg: '50%', xs: '100%' },
-        }}>
-        <Typography
-          variant='h1'
-          sx={{
-            texttransform: 'uppercase',
-            fontWeight: '1000',
-            textAlign: { lg: 'left', sm: 'center' },
-            fontSize: { xs: '2.5rem', sm: '2.5rem', md: '3rem', lg: '4rem' },
+            overflow: 'hidden',
           }}>
-          EXPLORE THE MOTORCYCLES
-        </Typography>
+          <Box
+            component='img'
+            src='images/motorbike.jpg'
+            alt='Home Image'
+            sx={{
+              width: '100%',
+              height: '100vh',
+              objectFit: 'cover',
+            }}
+            className='models__image'
+          />
+        </Box>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: { lg: '30%', xs: '50%' },
+            transform: 'translate(-50%, -50%)',
+            color: 'white',
+            px: 2,
+            width: { lg: '50%', xs: '100%' },
+          }}>
+          <Typography
+            variant='h1'
+            sx={{
+              textTransform: 'uppercase',
+              fontWeight: '1000',
+              fontSize: { xs: '2.5rem', sm: '2.5rem', md: '3rem', lg: '4rem' },
+              textAlign: { lg: 'left', xs: 'center' },
+            }}>
+            EXPLORE THE MOTORCYCLES
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
