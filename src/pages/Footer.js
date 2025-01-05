acf6b8c933c4577ee5548d12f0a8aa7599b03b85
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-01-05 21:56:39 +0000

    fix: footer

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,73 +1,77 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import { Facebook, Instagram, Twitter } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { LinkedIn, GitHub } from "@mui/icons-material";
+import { Box } from "@mui/material";
 
 function Footer() {
   return (
     <Box
-      component='footer'
+      component="footer"
       sx={{
-        backgroundColor: '#0A0A0A',
-        color: '#ffffff',
+        backgroundColor: "#0A0A0A",
+        color: "#ffffff",
 
         p: 6,
-      }}>
-      <Container maxWidth='lg'>
+      }}
+    >
+      <Container maxWidth="lg">
         <Grid container spacing={5}>
           <Grid item xs={12} sm={4}>
-            <Typography variant='h6' color='#ffffff' gutterBottom>
-              About Us
+            <Typography variant="h6" color="#ffffff" gutterBottom>
+              About
             </Typography>
-            <Typography variant='body2' color='#ffffff'>
-              This web site is a project developed for the learning purpose.
+            <Typography variant="body2" color="#ffffff">
+              This website is my personal project, developed for learning
+              purposes.
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Typography variant='h6' color='#ffffff' gutterBottom>
-              Contact Us
+            <Typography variant="h6" color="#ffffff" gutterBottom>
+              Contact
             </Typography>
-            <Typography variant='body2' color='#ffffff'>
+            <Typography variant="body2" color="#ffffff">
               London, UK
             </Typography>
-            <Typography variant='body2' color='#ffffff'>
-              Email: info@example.com
+            <Typography variant="body2" color="#ffffff">
+              Email: kronverk01@gmail.com
             </Typography>
-            <Typography variant='body2' color='#ffffff'>
-              Phone: +44 234 567 8901
+            <Typography variant="body2" color="#ffffff">
+              Phone: better to send an email
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Typography variant='h6' color='#ffffff' gutterBottom>
-              Follow Us
+            <Typography variant="h6" color="#ffffff" gutterBottom>
+              Follow
             </Typography>
-            <Link href='https://www.facebook.com/' color='inherit'>
-              <Facebook />
+            <Link
+              href="https://github.com/OlenaReukova"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <GitHub />
             </Link>
             <Link
-              href='https://www.instagram.com/'
-              color='inherit'
-              sx={{ pl: 1, pr: 1 }}>
-              <Instagram />
-            </Link>
-            <Link href='https://www.twitter.com/' color='inherit'>
-              <Twitter />
+              href="https://www.linkedin.com/in/olenareukova"
+              color="inherit"
+            >
+              <LinkedIn />
             </Link>
           </Grid>
         </Grid>
         <Box mt={5}>
-          <Typography variant='body2' color='#ffffff' align='center'>
-            {'© '}
+          <Typography variant="body2" color="#ffffff" align="center">
+            {"© "}
             <Link
-              color='inherit'
-              href='https://www.linkedin.com/in/olenareukova'>
+              color="inherit"
+              href="https://www.linkedin.com/in/olenareukova"
+            >
               Created by Olena Reukova
-            </Link>{' '}
+            </Link>{" "}
             {new Date().getFullYear()}
-            {'.'}
+            {"."}
           </Typography>
         </Box>
       </Container>
