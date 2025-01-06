62699632eb46191011c3d17616b92217761366c2
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-01-06 16:42:35 +0000

    fix: Model route, layout overflow issue
    The issue was caused by the default negative margins applied to the Grid component in Material-UI (Unstable_Grid2) when spacing is used. This caused horizontal overflow on mobile screen, leading to the page moving left and right.
    
    Adding sx={{ height: '100%', margin: 0, width: '100%' }}

--- src/ModelsExplorerTwoCards.js
+++ src/ModelsExplorerTwoCards.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import { Link } from "react-router-dom";
 
 export default function ModelsExplorerTwoCards() {
   return (
-    <Box sx={{ background: 'white' }}>
+    <Box sx={{ background: "white" }}>
       <Box
-        sx={{ flexGrow: 1, pt: 8, flexShrink: 0, background: 'white' }}
-        minHeight={600}>
-        <Grid container spacing={2} minHeight={600}>
+        sx={{ flexGrow: 1, pt: 8, flexShrink: 0, background: "white" }}
+        minHeight={600}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{ height: "100%", margin: 0, width: "100%" }}
+        >
           <Grid
             xs={12}
             sm={12}
             md={6}
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Card>
               <CardMedia
-                sx={{ objectFit: 'cover' }}
-                component='img'
-                width={'100%'}
-                alt='Explorer Models'
-                height='400'
-                image='/images/triumph_daytona.webp'
+                sx={{ objectFit: "cover" }}
+                component="img"
+                width={"100%"}
+                alt="Explorer Models"
+                height="400"
+                image="/images/triumph_daytona.webp"
               />
               <CardContent>
                 <Typography
                   gutterBottom
-                  variant='h3'
-                  component='div'
-                  sx={{ textTransform: 'uppercase' }}>
+                  variant="h3"
+                  component="div"
+                  sx={{ textTransform: "uppercase" }}
+                >
                   Sport
                 </Typography>
-                <Typography variant='body2' color='text.secondary'>
+                <Typography variant="body2" color="text.secondary">
                   Discover the detail in the Daytona DNA. Dynamic sports
                   attitude for every road and every ride, with head-turning
                   style, intuitive agility and thrilling performance to put you
@@ -53,20 +60,21 @@ export default function ModelsExplorerTwoCards() {
               <CardActions>
                 <Button
                   component={Link}
-                  to='/models'
-                  size='small'
+                  to="/models"
+                  size="small"
                   endIcon={<ArrowRightAltIcon />}
                   sx={{
-                    color: 'black',
-                    fontWeight: 'bold',
-                    textTransform: 'uppercase',
+                    color: "black",
+                    fontWeight: "bold",
+                    textTransform: "uppercase",
                     padding: 0,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                      backgroundColor: 'transparent',
-                      color: 'black',
+                    backgroundColor: "white",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                      color: "black",
                     },
-                  }}>
+                  }}
+                >
                   Explorer Sport
                 </Button>
               </CardActions>
@@ -77,27 +85,30 @@ export default function ModelsExplorerTwoCards() {
             sm={12}
             md={6}
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Card>
               <CardMedia
-                sx={{ objectFit: 'cover' }}
-                component='img'
-                alt='Offers and Finance'
-                height='400'
-                image='/images/triumph_steeladdition.webp'
+                sx={{ objectFit: "cover" }}
+                component="img"
+                alt="Offers and Finance"
+                height="400"
+                width="100%"
+                image="/images/triumph_steeladdition.webp"
               />
               <CardContent>
                 <Typography
                   gutterBottom
-                  variant='h3'
-                  component='div'
-                  sx={{ textTransform: 'uppercase' }}>
+                  variant="h3"
+                  component="div"
+                  sx={{ textTransform: "uppercase" }}
+                >
                   STEALTH EDITIONS
                 </Typography>
-                <Typography variant='body2' color='text.secondary'>
+                <Typography variant="body2" color="text.secondary">
                   Triumph Stealth Editions – a collection of eight new modern
                   classics showcasing dramatic and unique hand-crafted paint
                   finishes, adding a contemporary twist to Triumph’s iconic
@@ -106,19 +117,20 @@ export default function ModelsExplorerTwoCards() {
               </CardContent>
               <CardActions>
                 <Button
-                  size='small'
+                  size="small"
                   endIcon={<ArrowRightAltIcon />}
                   sx={{
-                    color: 'black',
-                    fontWeight: 'bold',
-                    textTransform: 'uppercase',
+                    color: "black",
+                    fontWeight: "bold",
+                    textTransform: "uppercase",
                     padding: 0,
-                    backgroundColor: 'white',
-                    '&:hover': {
-                      backgroundColor: 'transparent',
-                      color: 'black',
+                    backgroundColor: "white",
+                    "&:hover": {
+                      backgroundColor: "transparent",
+                      color: "black",
                     },
-                  }}>
+                  }}
+                >
                   Explorer STEALTH EDITIONS
                 </Button>
               </CardActions>
